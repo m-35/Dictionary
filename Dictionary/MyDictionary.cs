10ab820c245c90a9073c1169ebef478cd58703fe
miray kozlu <mikoz1@live.com>
2021-01-23 02:23:13 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] itemss;
+        T2[] itemm;
+
+        public MyDictionary()
+        {
+            itemss = new T1[0];
+            itemm = new T2[0];
+
+        }
+        public void Add(T1 items , T2 item)
+        {
+            T1[] tempArray = itemss ;
+            T2[] tempArray1 = itemm;
+
+            itemss = new T1[itemss.Length + 1];
+            itemm = new T2[itemm.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                itemss[i] = tempArray[i];
+                itemm[i] = tempArray1[i];
+            }
+           
+            itemss[itemss.Length - 1] = items;
+            itemm[itemm.Length - 1] = item;
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> customer = new Dictionary<int, string>();
+            customer.Add(1,"Miray KOZLU");
+            customer.Add(2,"Seda AKÇA");
+            customer.Add(3, "Esra KETE");
+            customer.Add(4, "Hasan BİLGE");
+            customer.Add(5, "KAAN TİRE");
+            customer.Add(6, "Ayşe TATLICI");
+
+            foreach (var customerNumber in customer.Keys)
+            {
+                Console.WriteLine(customerNumber);
+            }
+            foreach (var customerName in customer.Values)
+            {
+                Console.WriteLine(customerName);
+            }
+        }
+    }
+}
